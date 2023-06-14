1b0270522e01c873dc46f8354a2ae0c016f64aa7
nesharp <nesterukmisha27@gmail.com>
2023-06-14 11:13:41 +0300

    base-settings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import logo from './favicon.svg'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				<link
+					rel='icon'
+					type='image/svg+xml'
+					sizes='any'
+					href='https://www.svgrepo.com/show/217771/shopping-logo.svg'
+				/>
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+  transparent: twColors.transparent,
+  black: "#2C2F35",
+  white: twColors.transparent,
+  primary: '#1F2125',
+  grey: '#1F2125',
+  t_blue: '#1D64EC',
+  t_green: '#0B9A55',
+  secondary: "#FF9902"
+
+
+}
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,7 +26,30 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        animationOpacity: {
+          from: { opacity: 0.2 },
+          to: { opacity: 1 }
+        },
+        scaleIn: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(0.9)',
+          },
+          '50%': {
+            opacity: 0.3,
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'scale(1)',
+          }
+        }
+      },
+      animation: {
+        opacity: 'animationOpacity .5s ease-in-out',
+        scaleIn: 'scaleIn .35s ease-in-out',
+      }
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
