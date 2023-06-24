c2d7f9dbb29fbcc12afc2ffa81c0832b4a56a35b
Michael <nesterukmisha27@gmail.com>
2023-06-24 15:54:44 +0300

    checkRole-added

--- src/providers/auth-provider/CheckRole.tsx
+++ src/providers/auth-provider/CheckRole.tsx
@@ -1,11 +1,20 @@
 import { useAuth } from '@/hooks/useAuth'
 import { TypeComponentAuthFields } from '../auth-page.types'
 import { FC, PropsWithChildren } from 'react'
+import { useRouter } from 'next/router'
 
 const CheckRole: FC<PropsWithChildren<TypeComponentAuthFields>> = (
-	{ component: { isOnlySignedIn } },
-	children
+    { component: { isOnlySignedIn } },
+    children
 ) => {
     const user = useAuth()
-    return 0
+    const router = useRouter()
+    if (isOnlySignedIn && user || !isOnlySignedIn) {
+        return <>{children}</>
+    }    
+    router.pathname !== '/auth' && router.push('/auth')
+
+    return null
+
 }
+export default CheckRole
